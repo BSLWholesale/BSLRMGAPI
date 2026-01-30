22e5f4d6c4114a365f97f071a5c1c98c0a0c892e
Kanchan <banswaraws@gmail.com>
2026-01-30 18:33:55 +0530

    Added an objects in clsEmployee class - Kanchan

--- Models/clsEmployee.cs
+++ Models/clsEmployee.cs
@@ -44,8 +44,12 @@ namespace BSLDaman.Models
         public string AssessmentYear { get; set; }
         public string EmpGender { get; set; }
         public string EmpRole { get; set; }
+        public string OperatorType { get; set; }
+        public string DeviceId { get; set; }
         public string QueryType { get; set; }
         public string vErrorMsg { get; set; }
+        public int vErrorCode { get; set; }
+
     }
 
 
