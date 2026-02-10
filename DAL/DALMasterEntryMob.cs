0dab5c4f6edcab74c8cedf6ae465100fe425623e
Kanchan <banswaraws@gmail.com>
2026-02-10 16:25:23 +0530

    Added the new API functions Fn_Get_ActiveBundle() and Fn_Update_BundleID_By_EmpID() in  DALMasterEntryMob.cs - Kanchan

--- DAL/DALMasterEntryMob.cs
+++ DAL/DALMasterEntryMob.cs
@@ -90,40 +90,117 @@ namespace BSLDaman.DAL
         }
 
 
-        
-        //public List<clsLine> Fn_Get_ActiveLine(clsLine objReq)
-        //{
-        //    var objResp = new List<clsLine>();
-        //    var obj = new clsLine();
-        //    try
-        //    {
-        //        if (Con.State == ConnectionState.Broken)
-        //        { Con.Close(); }
-        //        if (Con.State == ConnectionState.Closed)
-        //        { Con.Open(); }
+        public List<clsBundleCompile> Fn_Get_ActiveBundle(clsBundleCompile objReq)
+        {
+            var objResp = new List<clsBundleCompile>();
+            var obj = new clsBundleCompile();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
 
-        //        SqlCommand cmd = new SqlCommand("", Con);
-        //        cmd.CommandType = CommandType.StoredProcedure;
+                SqlCommand cmd = new SqlCommand("USP_MobileBundleApp", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@QueryType", "GetActiveBundle");
 
-        //        SqlDataAdapter da = new SqlDataAdapter(cmd);
-        //        DataSet ds = new DataSet();
-        //        da.Fill(ds);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                int i = 0;
 
-        //        int i = 0;
-        //        if (ds.Tables[0].Rows.Count > 0)
-        //        {
-        //            while (ds.Tables[0].Rows.Count > i)
-        //            {
-        //                var objItem = new clsLine();
-        //                objItem.
-        //            }
-        //        }
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    while (ds.Tables[0].Rows.Count > i)
+                    {
+                        obj = new clsBundleCompile();
+                        obj.BundleID = Convert.ToInt64(ds.Tables[0].Rows[i]["BundleID"]);
+                        obj.BundleNo = Convert.ToInt32(ds.Tables[0].Rows[i]["BundleNo"]);
+                        obj.SizeName = Convert.ToString(ds.Tables[0].Rows[i]["SizeName"]);
+                        obj.ShadeSelectionID = Convert.ToInt32(ds.Tables[0].Rows[i]["ShadeSelectionID"]);
+                        obj.ColorName = Convert.ToString(ds.Tables[0].Rows[i]["ColorName"]);
+                        obj.ShadeName = Convert.ToString(ds.Tables[0].Rows[i]["ShadeName"]);
+                        obj.Qty = Convert.ToInt32(ds.Tables[0].Rows[i]["Qty"]);
+                        obj.PlyTo = Convert.ToInt32(ds.Tables[0].Rows[i]["PlyTo"]);
+                        obj.PlyFrom = Convert.ToInt32(ds.Tables[0].Rows[i]["PlyFrom"]);
+                        obj.SubSection = Convert.ToString(ds.Tables[0].Rows[i]["SubSection"]);
+                        obj.IsDispatch = Convert.ToBoolean(ds.Tables[0].Rows[i]["Dispatch"]);
+                        obj.StyleCode = Convert.ToString(ds.Tables[0].Rows[i]["StyleCode"]);
+                        obj.OrderNo = Convert.ToString(ds.Tables[0].Rows[i]["OrderNo"]);
 
-                 
+                        obj.vErrorCode = 200;
+                        obj.vErrorMsg = "Success";
+                        objResp.Add(obj);
+                        i++;
+                    }
+                }
+                else
+                {
+                    obj.vErrorCode = 404;
+                    obj.vErrorMsg = "No records are found.";
+                    objResp.Add(obj);
+                }
+            }
+            catch (Exception exp)
+            {
+                obj.vErrorCode = 500;
+                Logger.WriteLog("Function Name : Fn_Get_ActiveBundle", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                obj.vErrorMsg = exp.Message.ToString();
+                objResp.Add(obj);
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
 
-        //    }
-        //}
 
+
+        public clsBundleCompile Fn_Update_BundleID_By_EmpID(clsBundleCompile objReq)
+        {
+            var objResp = new clsBundleCompile();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
+
+                SqlCommand cmd = new SqlCommand("USP_MobileBundleApp", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@AppEmpID", objReq.AppEmpID);
+                cmd.Parameters.AddWithValue("@AppStartTime", objReq.AppStartTime);
+                cmd.Parameters.AddWithValue("@AppEndTime", objReq.AppEndTime);
+                cmd.Parameters.AddWithValue("@ModifiedBy", objReq.ModifiedBy);
+                cmd.Parameters.AddWithValue("@QueryType", "UpdateBundleDetails");
+
+                int i = 0;
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    objResp.vErrorCode = 200;
+                    objResp.vErrorMsg = "Success";
+                }
+                else
+                {
+                    objResp.vErrorCode = 400;
+                    objResp.vErrorMsg = "Updating Failed.";
+                }
+            }
+            catch (Exception exp)
+            {
+                objResp.vErrorCode = 500;
+                Logger.WriteLog("Function Name : Fn_Update_BundleID_By_EmpID", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
 
 
     }
