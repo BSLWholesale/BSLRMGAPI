1ef21084195fcbb8308041d8d6717f8ec0697079
Kanchan <banswaraws@gmail.com>
2026-02-13 12:51:17 +0530

    Added the new Controller MOBEmployeeController.cs and MOBProductionController.cs - Kanchan

---
+++ Controllers/MOBEmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using BSLDaman.DAL;
+using BSLDaman.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BSLDaman.Controllers
+{
+    public class MOBEmployeeController : Controller
+    {
+        // GET: MOBEmployee
+
+        MOBDALEmployee _MOBDALEmployee = new MOBDALEmployee();
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/EmployeeMob/Fn_Insert_Employee")]
+        public clsEmployee Fn_Insert_Employee(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            objResp = _MOBDALEmployee.Fn_Insert_Employee(objReq);
+            return objResp;
+        }
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/EmployeeMob/Fn_Login_Employee")]
+        public clsEmployee Fn_Login_Employee(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            objResp = _MOBDALEmployee.Fn_Login_Employee(objReq);
+            return objResp;
+        }
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/EmployeeMob/Fn_Fetch_EmployeeDetail_ById")]
+        public clsEmployee Fn_Fetch_EmployeeDetail_ById(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            objResp = _MOBDALEmployee.Fn_Fetch_EmployeeDetail_ById(objReq);
+            return objResp;
+        }
+
+
+    }
+}
---
+++ Controllers/MOBProductionController.cs
@@ -0,0 +1,41 @@
+﻿using BSLDaman.DAL;
+using BSLDaman.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace BSLDaman.Controllers
+{
+    public class MOBProductionController : ApiController
+    {
+        // GET: MOBProduction
+
+        MOBDALProduction _MOBDALProduction = new MOBDALProduction();
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/MasterEntryMob/Fn_Get_ActiveBundle")]
+        public List<clsBundleCompile> Fn_Get_ActiveBundle(clsBundleCompile objReq)
+        {
+            var objResp = new List<clsBundleCompile>();
+            objResp = _MOBDALProduction.Fn_Get_ActiveBundle(objReq);
+            return objResp;
+        }
+
+
+        [System.Web.Http.HttpPut]
+        [System.Web.Http.Route("api/MasterEntryMob/Fn_Update_BundleID_By_EmpID")]
+        public clsBundleCompile Fn_Update_BundleID_By_EmpID(clsBundleCompile objReq)
+        {
+            var objResp = new clsBundleCompile();
+            objResp = _MOBDALProduction.Fn_Update_BundleID_By_EmpID(objReq);
+            return objResp;
+        }
+
+
+    }
+}
