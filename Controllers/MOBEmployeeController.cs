141bbd82edabd796f8b91740af97e45ba2d4a5c9
Kanchan <banswaraws@gmail.com>
2026-02-13 14:26:56 +0530

    Modified in MOBEmployeeController.cs API keyword added - Kanchan

--- Controllers/MOBEmployeeController.cs
+++ Controllers/MOBEmployeeController.cs
@@ -9,7 +9,7 @@ using System.Web.Mvc;
 
 namespace BSLDaman.Controllers
 {
-    public class MOBEmployeeController : Controller
+    public class MOBEmployeeController : ApiController
     {
         // GET: MOBEmployee
 
