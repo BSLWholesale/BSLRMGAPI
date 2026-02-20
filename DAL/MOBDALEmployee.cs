452d0bd4406c0cb376701af2cb27b87d1d6f1cae
Kanchan <banswaraws@gmail.com>
2026-02-20 17:33:24 +0530

    Added the function i.e. Fn_Generate_EmployeeTokenId() and remove the function i.e. Fn_Insert_Employee() MOBDALEmployee.cs - Kanchan

--- DAL/MOBDALEmployee.cs
+++ DAL/MOBDALEmployee.cs
@@ -17,113 +17,6 @@ namespace BSLDaman.DAL
 
         SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["BSL"].ConnectionString);
 
-
-        public clsEmployee Fn_Insert_Employee(clsEmployee objReq)
-        {
-            var objResp = new clsEmployee();
-            try
-            {
-                if (objReq.nEmpId == null || objReq.nEmpId == 0)
-                {
-                    objResp.vErrorMsg = "Please Enter an Employee ID";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.vEmpName))
-                {
-                    objResp.vErrorMsg = "Please Enter the Employee Name";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.EmpGender))
-                {
-                    objResp.vErrorMsg = "Please Select the Gender";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.vEmpGrade))
-                {
-                    objResp.vErrorMsg = "Please Enter the Grade";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.vEmpPassword))
-                {
-                    objResp.vErrorMsg = "Please Enter the Password";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.OperatorType))
-                {
-                    objResp.vErrorMsg = "Please Enter the Operator Type";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.vEmpMobile))
-                {
-                    objResp.vErrorMsg = "Please Enter the Mobile Number";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.EmpRole))
-                {
-                    objResp.vErrorMsg = "Please Enter the Employee Role";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.vEmpLocation))
-                {
-                    objResp.vErrorMsg = "Please Enter the Employee Location";
-                    objResp.vErrorCode = 300;
-                }
-                else if (String.IsNullOrWhiteSpace(objReq.DeviceId))
-                {
-                    objResp.vErrorMsg = "Please Enter the Device ID";
-                    objResp.vErrorCode = 300;
-                }
-                else
-                {
-                    if (Con.State == ConnectionState.Broken)
-                    { Con.Close(); }
-                    if (Con.State == ConnectionState.Closed)
-                    { Con.Open(); }
-
-                    string encryptPassword = Generic.EncryptText(objReq.vEmpPassword);
-                    SqlCommand cmd = new SqlCommand("USP_EmployeeMob", Con);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@EmpId", objReq.nEmpId);
-                    cmd.Parameters.AddWithValue("@EmpName", objReq.vEmpName);
-                    cmd.Parameters.AddWithValue("@EmpGender", objReq.EmpGender);
-                    cmd.Parameters.AddWithValue("@EmpGrade", objReq.vEmpGrade);
-                    cmd.Parameters.AddWithValue("@EmpPassword", encryptPassword);
-                    cmd.Parameters.AddWithValue("@OperatorType", objReq.OperatorType);
-                    cmd.Parameters.AddWithValue("@EmpMobile", objReq.vEmpMobile);
-                    cmd.Parameters.AddWithValue("@EmpRole", objReq.EmpRole);
-                    cmd.Parameters.AddWithValue("@EmpLocation", objReq.vEmpLocation);
-                    cmd.Parameters.AddWithValue("@DeviceId", objReq.DeviceId);
-                    cmd.Parameters.AddWithValue("@QueryType", "Insert");
-
-                    int i = 0;
-                    i = cmd.ExecuteNonQuery();
-                    if (i > 0)
-                    {
-                        objResp.vErrorMsg = "Success";
-                        objResp.vErrorCode = 200;
-                    }
-                    else
-                    {
-                        objResp.vErrorMsg = "Employee Registration Failed.";
-                        objResp.vErrorCode = 300;
-                    }
-                }
-            }
-            catch (Exception exp)
-            {
-                Logger.WriteLog("Function Name : Fn_Insert_Employee", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
-                objResp.vErrorMsg = exp.Message.ToString();
-                objResp.vErrorCode = 500;
-            }
-            finally
-            {
-                Con.Close();
-            }
-            return objResp;
-        }
-
-
-
         public clsEmployee Fn_Login_Employee(clsEmployee objReq)
         {
             var objResp = new clsEmployee();
@@ -168,8 +61,14 @@ namespace BSLDaman.DAL
                         objResp.DeviceId = Convert.ToString(ds.Tables[0].Rows[i]["DeviceId"]);
                         objResp.vEmpPassword = decryptPassword;
 
+                        if (objReq.TokenId == null || objReq.TokenId == "")
+                        {
+                            objResp.TokenId = Fn_Generate_EmployeeTokenId(Convert.ToInt64(objReq.nEmpId), objReq.TokenId);
+                        }
+
                         objResp.vErrorMsg = "Success";
                         objResp.vErrorCode = 200;
+
                     }
                     else
                     {
@@ -244,6 +143,62 @@ namespace BSLDaman.DAL
             return objResp;
         }
 
+
+        public string Fn_Generate_EmployeeTokenId(Int64 EmpId, string TokenId)
+        {
+            var objResp = new clsEmployee();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
+
+                Random random = new Random();
+                var randomNumber = random.Next(1, 1000001);
+
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                var stringChars = new char[15];
+                for (int j = 0; j < stringChars.Length; j++)
+                {
+                    stringChars[j] = chars[random.Next(chars.Length)];
+                }
+
+                string finalString = new String(stringChars);
+
+                TokenId = finalString;
+
+                SqlCommand cmd = new SqlCommand("USP_EmployeeMob", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmpId", EmpId);
+                cmd.Parameters.AddWithValue("@TokenId", TokenId);
+                cmd.Parameters.AddWithValue("@QueryType", "UpdateTokenId");
+
+                int i = 0;
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    objResp.vErrorMsg = "Success";
+                    objResp.vErrorCode = 200;                     
+                }
+                else
+                {
+                    objResp.vErrorMsg = "Employee token genration failed.";
+                    objResp.vErrorCode = 300;
+                }
+            }
+            catch (Exception exp)
+            {
+                Logger.WriteLog("Function Name : Fn_Generate_EmployeeTokenId", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+                objResp.vErrorCode = 500;
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return TokenId;
+        }
 
 
     }
