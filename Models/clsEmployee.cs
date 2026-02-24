0891356244eedf6213d75896827df84a693b7ab9
Kanchan <banswaraws@gmail.com>
2026-02-24 14:42:57 +0530

    Modified in clsEmployee.cs added an objects in the class i.e. clsBundleCompile - Kanchan

--- Models/clsEmployee.cs
+++ Models/clsEmployee.cs
@@ -346,9 +346,11 @@ namespace BSLDaman.Models
         public string CreatedOn { get; set; }
         public int ModifiedBy { get; set; }
         public string ModifiedOn { get; set; }
+        public int SupervisorEmpID { get; set; }
         public int AppEmpID { get; set; }
         public string AppStartTime { get; set; }
         public string AppEndTime { get; set; }
+        public string BundleIDStatus { get; set; }
         public string vErrorMsg { get; set; }
         public int vErrorCode { get; set; }
     }
