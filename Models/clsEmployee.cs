c04d5592fb3c5053bb11e6c38665439671a568a1
Kanchan <banswaraws@gmail.com>
2026-03-10 11:45:23 +0530

    Modified in clsEmployee.cs added an object in class clsMOBEmployee - Kanchan

--- Models/clsEmployee.cs
+++ Models/clsEmployee.cs
@@ -429,6 +429,7 @@ namespace BSLDaman.Models
         public bool IsActive { get; set; }
         public Int64 EmpOperatorCount { get; set; }
         public string EmpOperatorStatus { get; set; }
+        public string LineName { get; set; }
         public string vErrorMsg { get; set; }
         public int vErrorCode { get; set; }
 
