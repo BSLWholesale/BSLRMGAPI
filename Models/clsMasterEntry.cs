9d5498aa72f68e3b8223a61998def4f333c2ce8e
Kanchan <banswaraws@gmail.com>
2026-03-20 18:15:44 +0530

    Modified in clsMasterEntry.cs added the new object i.e. FinishedQty in clsLine class - Kanchan

--- Models/clsMasterEntry.cs
+++ Models/clsMasterEntry.cs
@@ -69,6 +69,7 @@ namespace BSLDaman.Models
         public string Hours { get; set; }
         public string Minutes { get; set; }
         public string OrderNo { get; set; }
+        public int FinishedQty { get; set; }
         public string vErrorMsg { get; set; }
         public int vErrorCode { get; set; }
     }
