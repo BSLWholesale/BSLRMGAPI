51f79c11dffc982d66fed4073f4d94516b887149
Kanchan <banswaraws@gmail.com>
2026-03-30 17:41:24 +0530

    Modified in clsEmployee.cs added an UpdateType object in clsBundleCompile class - Kanchan

--- Models/clsEmployee.cs
+++ Models/clsEmployee.cs
@@ -362,6 +362,7 @@ namespace BSLDaman.Models
         public Int64 LineId { get; set; }
         public string LineStatus { get; set; }
         public Int64 OperationNo { get; set; }
+        public string UpdateType { get; set; }
         public string vErrorMsg { get; set; }
         public int vErrorCode { get; set; }
     }
