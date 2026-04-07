71acc836e6ce45ead5a8bebbd0382ad05bcfa141
Ankit <banswaraws@gmail.com>
2026-04-07 16:35:19 +0530

    Created API and function Fn_Get_Bundle_Report for display bundle report

--- Controllers/ProductionController.cs
+++ Controllers/ProductionController.cs
@@ -286,5 +286,19 @@ namespace BSLDaman.Controllers
             objResp = _DALProduction.Fn_Get_Subsection_List(objReq);
             return objResp;
         }
+
+
+        #region Start Fn_Get_Bundle_Report 96-APR-2026
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/Production/Fn_Get_Bundle_Report")]
+        public List<clsBundleCompile> Fn_Get_Bundle_Report(clsBundleCompile objReq)
+        {
+            var objResp = new List<clsBundleCompile>();
+            objResp = _DALProduction.Fn_Get_Bundle_Report(objReq);
+            return objResp;
+        }
+
+        #endregion End Fn_Get_Bundle_Report 96-APR-2026
     }
 }
