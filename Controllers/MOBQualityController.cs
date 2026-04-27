363cdf4890f2d5c6740c08e725d033e2f7d577e3
Kanchan <banswaraws@gmail.com>
2026-04-27 16:47:46 +0530

    New MOBQualityController.cs and data access layer i.e. MOBDALQuality.cs added - Kanchan

---
+++ Controllers/MOBQualityController.cs
@@ -0,0 +1,31 @@
+﻿using BSLDaman.DAL;
+using BSLDaman.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BSLDaman.Controllers
+{
+    public class MOBQualityController : Controller
+    {
+
+        MOBDALQuality _MOBDALQuality = new MOBDALQuality();
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/MOBQuality/Fn_Fetch_AllOrderNumbers")]
+        public List<clsOrderMaster> Fn_Fetch_AllOrderNumbers(clsOrderMaster objReq)
+        {
+            var objResp = new List<clsOrderMaster>();
+            objResp = _MOBDALQuality.Fn_Fetch_AllOrderNumbers(objReq);
+            return objResp;
+        }
+
+
+
+
+    }
+}
---
+++ DAL/MOBDALQuality.cs
@@ -0,0 +1,74 @@
+﻿using BSLDaman.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace BSLDaman.DAL
+{
+    public class MOBDALQuality
+    {
+        
+        SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["BSL"].ConnectionString);
+
+
+        public List<clsOrderMaster> Fn_Fetch_AllOrderNumbers(clsOrderMaster objReq)
+        {
+            var objResp = new List<clsOrderMaster>();
+            var obj = new clsOrderMaster();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
+
+                SqlCommand cmd = new SqlCommand("USP_MobileQualityAnalysis", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                int i = 0;
+
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    while (ds.Tables[0].Rows.Count > i)
+                    {
+                        obj = new clsOrderMaster();
+                        obj.OrderNo = Convert.ToString(ds.Tables[0].Rows[i]["OrderNo"]);
+
+                        obj.vErrorCode = 200;
+                        obj.vErrorMsg = "Success";
+                        objResp.Add(obj);
+                        i++;
+                    }
+                }
+                else
+                {
+                    obj.vErrorCode = 404;
+                    obj.vErrorMsg = "Order Numbers records are not found.";
+                    objResp.Add(obj);
+                }
+            }
+            catch (Exception exp)
+            {
+                obj.vErrorCode = 500;
+                Logger.WriteLog("Function Name : ", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                obj.vErrorMsg = exp.Message.ToString();
+                objResp.Add(obj);
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
+
+
+    }
+}
