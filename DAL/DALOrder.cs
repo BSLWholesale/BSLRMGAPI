f78df471c4794f38d7c43c50ad422a1a18b4aae8
Ankit <banswaraws@gmail.com>
2026-06-19 12:36:51 +0530

    Updated Fn_Get_Operation_BreackdownFile function and sql query order by

--- DAL/DALOrder.cs
+++ DAL/DALOrder.cs
@@ -688,7 +688,7 @@ namespace BSLDaman.DAL
                 {
                     strSql = strSql + " AND OD.OpNo = @OpNo ";
                 }
-                strSql = strSql + " ORDER BY OD.SubSection ASC ";
+                strSql = strSql + " ORDER BY OD.SeqNo, OD.SubSection ASC ";
 
                 SqlCommand cmd = new SqlCommand(strSql, Con);
                 cmd.CommandType = CommandType.Text;
